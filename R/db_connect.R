65ed7bff476531fe8a9b7177e04a3959f800cbcf
charles <charles@charles.local>
2018-09-06 16:08:50 -0700

    add pricescout connection function

--- R/db_connect.R
+++ R/db_connect.R
@@ -45,3 +45,52 @@ snowflake_connect <- function(driver.path, user, pw) {
                      user, pw)
     return(conn)
 }
+
+#' Connect to Snowflake
+#'
+#' This function creates a Snowflake JDBC connection object for querying InfoScout tables directly from R.
+#' @param driver.path Local path to the Snowflake JDBC driver (.jar)
+#' @param user Database username
+#' @param pw Database password
+#' @return A Snowflake connection object
+#' @examples driver.path = '/users/me/documents/drivers/snowflake-jdbc-3.6.6.jar'
+#' @examples snowflake <- snowflake_connect(driver.path)
+#' @examples df <- dbGetQuery(snowflake, 'SELECT * FROM us_tlog.d_user LIMIT 10;')
+#' @export
+snowflake_connect <- function(driver.path, user, pw) {
+    # Creates a connection object to use when querying vertica
+    #
+    # Returns:
+    #   A snowflake connection object
+    
+    options( java.parameters = "-Xmx4g" )
+    vDriver <- RJDBC::JDBC(driverClass="net.snowflake.client.jdbc.SnowflakeDriver", classPath=driver.path)
+    conn = RJDBC::dbConnect(vDriver, "jdbc:snowflake://infoscout.snowflakecomputing.com/?warehouse=ISC_DW_XS&db=infoscout",
+                            user, pw)
+    return(conn)
+}
+
+#' Connect to Pricescout MYSQL DB
+#'
+#' This function creates a Snowflake JDBC connection object for querying InfoScout tables directly from R.
+#' @param user Database username
+#' @param pw Database password
+#' @return A MySQL connection object to pricescout DB
+#' @examples pc <- pricescout_connect(user, pw)
+#' @examples df <- dbGetQuery(pc, 'SELECT * FROM pricescout.rdl_receipt LIMIT 10;')
+#' @export
+pricescout_connect <- function(user, pw) {
+    # Creates a connection object to use when querying vertica
+    #
+    # Returns:
+    #   A mysql connection object
+    
+    conn <- RMySQL::dbConnect(RMySQL::MySQL(), 
+                              user=user, 
+                              password=pw, 
+                              port=3306, 
+                              dbname='pricescout', 
+                              host='prd-ca-rds-slave-01.infoscoutinc.net')
+    return(conn)
+}
+
