60129b5ab4e339c3288855dea3a6563ce24952e5
Charles Choe <charles@infoscoutinc.com>
2018-09-28 15:25:34 -0700

    Update db_connect.R
    remove duped function

--- R/db_connect.R
+++ R/db_connect.R
@@ -46,30 +46,6 @@ snowflake_connect <- function(driver.path, user, pw) {
     return(conn)
 }
 
-#' Connect to Snowflake
-#'
-#' This function creates a Snowflake JDBC connection object for querying InfoScout tables directly from R.
-#' @param driver.path Local path to the Snowflake JDBC driver (.jar)
-#' @param user Database username
-#' @param pw Database password
-#' @return A Snowflake connection object
-#' @examples driver.path = '/users/me/documents/drivers/snowflake-jdbc-3.6.6.jar'
-#' @examples snowflake <- snowflake_connect(driver.path)
-#' @examples df <- dbGetQuery(snowflake, 'SELECT * FROM us_tlog.d_user LIMIT 10;')
-#' @export
-snowflake_connect <- function(driver.path, user, pw) {
-    # Creates a connection object to use when querying vertica
-    #
-    # Returns:
-    #   A snowflake connection object
-    
-    options( java.parameters = "-Xmx4g" )
-    vDriver <- RJDBC::JDBC(driverClass="net.snowflake.client.jdbc.SnowflakeDriver", classPath=driver.path)
-    conn = RJDBC::dbConnect(vDriver, "jdbc:snowflake://infoscout.snowflakecomputing.com/?warehouse=ISC_DW_XS&db=infoscout",
-                            user, pw)
-    return(conn)
-}
-
 #' Connect to Pricescout MYSQL DB
 #'
 #' This function creates a Snowflake JDBC connection object for querying InfoScout tables directly from R.
