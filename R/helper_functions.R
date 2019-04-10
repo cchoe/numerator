6425218b877e10c217922c7c7c026fbd8cc0714d
Charles Choe <charles@infoscoutinc.com>
2019-04-10 15:41:12 -0700

    Update helper_functions.R
    update help page

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,6 +1,6 @@
 #' Operator NOT IN
 #'
-#' This operator is the opposite of the `in` operator
+#' This operator is the opposite of the 'in' operator
 #'
 #' @examples 1 %ni% 1  FALSE
 #' @examples 1 %ni% 2  TRUE
